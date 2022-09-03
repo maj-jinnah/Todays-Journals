a9a7ad64392cf6876c4c802ea7f8ffbaf653e56e
maj-jinnah <maj.jinnah@gmail.com>
2022-09-03 19:07:46 +0600

    categoty start

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,4 +1,4 @@
-console.log('Hi!');
+
 
 fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
@@ -19,11 +19,48 @@ const displayCategory = (categories, categories_id) => {
 
     categories_id.forEach(category_id => {
         const categoryId = category_id.category_id;
-        console.log(categoryId);
+        displayNewsId(categoryId);
     });
-
-
 }
-const displayNews = categoryId =>{
-    console.log(categoryId);
+const displayNewsId = categoryId => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNews(data.data))
+
+    const displayNews = (newses) => {
+        console.log(newses);
+        const newsArea = document.getElementById('newsarea');
+        newses.forEach(news => {
+            const newsDiv = document.createElement('div')
+            newsDiv.innerHTML = `
+                <div class="card mb-3 w-75">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${news.title}</h5>
+                                <p class="card-text" class="card-text" style="max-height: 400px;white-space: nowrap; overflow: hidden;  text-overflow: ellipsis">${news.details}</p> 
+                                <div class="card-text d-flex align-items-center mt-3">
+                                    <div class="d-flex align-items-center">
+                                        <img src="${news.author.img}" style="height: 40px;width: 40px;" class="rounded-circle"></img>
+                                        <div class="ms-3">
+                                            <h6>${news.author.name}</h4>
+                                            <p class = "m-0">${news.author.published_date}</p>
+                                        </div>
+                                    </div>
+                                    <h6 class="ms-5"> ${news.total_view}</h6>
+                                    <p><i class="fa-solid fa-eyes"></i></p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
+            newsArea.appendChild(newsDiv);
+        })
+    };
 }
